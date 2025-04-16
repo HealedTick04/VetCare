9324f8a6a558c5f7f49127ec60ad6032ab34ac07
Gaby0-0 <gabriela0203martinez@gmail.com>
2025-04-16 15:01:20 -0600

    Cambios de prueba2

--- VetCare/src/main/java/com/mycompany/vetcare/VetCare.java
+++ VetCare/src/main/java/com/mycompany/vetcare/VetCare.java
@@ -13,5 +13,6 @@ public class VetCare {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Esta es la prueba del pull!");
+        System.out.println("Esta es la prueba del pull2!");
     }
 }
