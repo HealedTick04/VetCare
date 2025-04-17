f26deaf391068fd8cb697f5dd30048956cac9ee0
Gaby0-0 <gabriela0203martinez@gmail.com>
2025-04-17 14:54:54 -0600

    Administrator class

---
+++ VetCare/src/main/java/VetCare/Entities/Administrator.java
@@ -0,0 +1,21 @@
+package VetCare.Entities;
+
+public class Administrator extends User {
+	private String id_RFC;
+	
+	public Administrator(String id_CURP, String password, String name, String lastName,String numberPhone, String email, String id_RFC) {
+	super(id_CURP, password, name, lastName, numberPhone, email);
+	this.id_RFC = id_RFC;
+	}
+	
+	public void setRFC(String id_RFC){
+		this.id_RFC = id_RFC;
+	}
+	
+	public String getRFC() {
+		return id_RFC;
+	}
+	
+	
+	
+}
