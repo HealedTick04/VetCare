bbdc91a69138343248495741b4d67980585b1e33
Fernando Rojas García <rojasgarciafernando2@gmail.com>
2025-05-17 14:30:20 -0600

    add new component customTextField

---
+++ VetCare/src/VetCare/Components/CustomTextField.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VetCare.Components;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author dary_
+ */
+public class CustomTextField extends JTextField{
+    
+}
